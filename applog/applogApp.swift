aeb4e2901e31ea1700d008c89d06adab3090b31b
Denis Sheremetov <denis.sheremetov@gmail.com>
2026-07-05 13:22:43 +0200

    Initial Commit

---
+++ applog/applogApp.swift
@@ -0,0 +1,17 @@
+//
+//  applogApp.swift
+//  applog
+//
+//  Created by Denis Sheremetov on 05/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct applogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
